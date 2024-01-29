c298bf25d5e0123bfc2e79ef6c8399fcbf223b03
AbdallaYoussef1 <abdalla.youssef182@gmail.com>
2024-01-30 00:04:58 +0300

    New Not Found page

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -10,27 +10,28 @@ import {
 //Components
 
 
-
-
 //MUI
 import {
-    Box,
+    Box, Typography,
 } from '@mui/material'
 import { styled } from '@mui/system'
 
 //Styled Components
 const StyledNotFoundPage = styled(Box)(
     ({ theme }) => ({
-    
+        textAlign: 'center',
+        padding: theme.spacing(2),
+        margin: '20px auto', // Add margin
     })
 )
 
 
 const NotFoundPage = () => {
     return (
-        <StyledNotFoundPage>
-            NotFoundPage
-        </StyledNotFoundPage>
+    <StyledNotFoundPage>
+        <Typography variant='h3'>{`404 - Not Found`}</Typography>
+        <Typography variant='body1'>Sorry, the page you are looking for does not exist.</Typography>
+    </StyledNotFoundPage>
     );
 };
 
